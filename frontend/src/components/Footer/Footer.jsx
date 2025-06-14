b02fba368e10ec13dc3025f15d02e6065eb7d5d8
Sidd123455 <146204163+Sidd123455@users.noreply.github.com>
2025-06-14 21:04:33 +0530

    Update Footer.jsx

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -11,8 +11,8 @@ const Footer = () => {
             <p>At BlinkBites, we're committed to delivering fresh, delicious food right to your doorstep — fast, safe, and hassle-free. Whether you're craving comfort meals or exploring new flavors, we've got you covered. Thank you for choosing us to serve your hunger, one bite at a time.</p>
             <div className="footer-social-icons">
                 <img src={assets.facebook_icon} alt="" />
-                <img src={assets.twitter_icon} alt="" />
-                <img src={assets.linkedin_icon} alt="" />
+                <img src={assets.twitter_icon} alt="https://x.com/SiddeshSapate?t=EZ4Ho7IHz_5kDVrsYYl2AA&s=09" />
+                <img src={assets.linkedin_icon} alt="https://www.linkedin.com/in/siddesh-s12/?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app" />
             </div>
         </div>
         <div className="footer-content-center">
